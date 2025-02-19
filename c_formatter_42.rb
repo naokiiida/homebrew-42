84d08bb3d41dd490a99f0ce1aa342635089db7b7
niida <54788301+naokiiida@users.noreply.github.com>
2025-02-19 19:54:56 +0900

    feat: test for c_formatter_42 package

--- c_formatter_42.rb
+++ c_formatter_42.rb
@@ -1,31 +1,29 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class CFormatter42 < Formula
   include Language::Python::Virtualenv
 
-  desc ""
-  homepage ""
-  url "https://files.pythonhosted.org/packages/b8/29/8cb9e92e65e462e5344cfde7607e1576839b3978235ba47d63276c9b1332/c_formatter_42-0.2.7.tar.gz"
-  sha256 "89e3edb7761f7461af03852e4f0ff3a00ffaec6fd83786810e1fae149bce20f2"
-  license ""
+  desc "C language formatter for 42 norminette"
+  homepage "https://github.com/dawnbeen/c_formatter_42"
+  url "https://files.pythonhosted.org/packages/77/fa/edee608023d6adc567d1060b4b889b6279e0f99ff3b3cc58bfa4eda069dd/c_formatter_42-0.2.8.tar.gz"
+  sha256 "da31b1ab4cdc92dd363ae791244cf063982e29c9fc54dbde0f22fce20a2b057b"
+  license "GPL-3.0"
 
-  depends_on "python"
+  depends_on "python" => ">=3.8"
 
   def install
     virtualenv_install_with_resources
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test c_formatter_42`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    (testpath/"test.c").write <<~EOS
+      int main()
+      {
+        return 0;
+      }
+    EOS
+    output = shell_output("#{bin}/c_formatter_42 #{testpath}/test.c", 0)
+    assert_match "Writing to #{testpath}/test.c", output
+    formatted_content = (testpath/"test.c").read
+    expected_output = "int\tmain(void)\n{\n\treturn (0);\n}\n"
+    assert_equal expected_output, formatted_content
   end
 end
