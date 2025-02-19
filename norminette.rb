a42cccff5c9f0430a15d03cf168595ac95698fc7
niida <54788301+naokiiida@users.noreply.github.com>
2025-02-19 19:15:46 +0900

    feat: working norminette brew package

--- norminette.rb
+++ norminette.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Norminette < Formula
   include Language::Python::Virtualenv
 
@@ -17,28 +14,19 @@ class Norminette < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test norminette`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    assert_match "norminette", shell_output("#{bin}/norminette --version", 0)
+    (testpath/"blank.c").write <<~EOF
+    EOF
+    output = shell_output("#{bin}/norminette blank.c")
+    assert_equal "blank.c: OK!", output.chomp
     (testpath/"test.c").write <<~EOF
       #include <stdio.h>
     EOF
-    output = shell_output("#{bin}/norminette test.c")
+    output = shell_output("#{bin}/norminette test.c", 1)
     expect = <<~EOF
       test.c: Error!
       Error: INVALID_HEADER       (line:   1, col:   1):	Missing or invalid 42 header
     EOF
-    assert_equal expect.chomp, output.chomp
-    assert_match "norminette", shell_output("#{bin}/norminette --version", 2)
-    (testpath/"blank.c").write <<~EOF
-        EOF
-    output = shell_output("#{bin}/norminette blank.c")
-    assert_match "blank.c: OK!", output.chomp
+    assert_equal expect, output
   end
 end
